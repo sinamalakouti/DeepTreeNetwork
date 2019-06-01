e27af85a34c4ae6f2857f9ed4396b63b1d06f489
sina <s.malakouti2015@aut.ac.ir>
2019-06-01 08:55:07 +0430

    debug MnistTest for stteing 40 * 784 40

--- controller/Network2.java
+++ controller/Network2.java
@@ -196,7 +196,7 @@ public class Network2 {
 //                mnistTest.reset();
                 //
                 String path =
-                        "/root/research/result/phase4/randomClassConfig/22/resultIteration_" + i;
+                        "/home/sina/eclipse-workspace/ComplexNeuronsProject/result/phase4/randomClassConfig/21/resultIteration_" + i;
                 File file = new File(path);
                 BufferedWriter out = new BufferedWriter(new
                         FileWriter(file));
