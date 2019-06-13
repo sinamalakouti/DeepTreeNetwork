706036cd67204e5064d99d2dd3b49e69c521128e
sina <s.malakouti2015@aut.ac.ir>
2019-06-13 19:55:25 +0430

    20n * 784/ 2

--- controller/Test.java
+++ controller/Test.java
@@ -142,7 +142,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		try{
-			_utils.createGNUPLOT_ds("net_60n_10dp", "/Users/sina/Documents/JGU_Research/ComplexNeuronsProject/Experiments/DeCoDeML Workshop/19/", 0, 110);
+			_utils.createGNUPLOT_ds("net_50n_10fr", "/Users/sina/Documents/JGU_Research/ComplexNeuronsProject/Experiments/DeCoDeML Workshop/8/", 0, 150);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
